f0caf685e205a0c3283876ea8de555d60dead1f8
corbs89 <ccmein@student.fullsail.edu>
2023-03-05 22:46:12 -0600

    player can sprint
    player can sprint and stamina updates properly

--- GDB229-O/Assets/Scripts/PlayerController.cs
+++ GDB229-O/Assets/Scripts/PlayerController.cs
@@ -9,9 +9,12 @@ public class PlayerController : MonoBehaviour
 
     [Header("-----Player Stats-----")]
     [SerializeField][Range(0f, 10f)] int HP;
-    [SerializeField][Range(0f, 10f)] int stamina;
+    [SerializeField][Range(0f, 100f)] float stamina;
     [SerializeField][Range(1f, 10f)] float playerSpeed;
     [SerializeField][Range(1f, 2f)] float sprintCoefficient;
+    [SerializeField][Range(1, 5)] float sprintCost;
+    [SerializeField][Range(1f, 10f)] float staminaRechargeRate;
+    [SerializeField][Range(1f, 10f)] float staminaRechargeStartTime;
     [SerializeField][Range(1, 5)] int jumpTimes;
     [SerializeField][Range(5, 50)] int jumpSpeed;
     [SerializeField][Range(10, 75)] int gravity;
@@ -24,18 +27,21 @@ public class PlayerController : MonoBehaviour
 
 
     int originalHP;
+    float staminaMax;
+    float timeSinceUsedStamina = Mathf.Infinity;
     int jumpsCurrent;
     Vector3 movement;
     Vector3 playerVelocity;
     int points;
     
     public int GetHP() { return HP; }
-    public int GetStamina() { return stamina; }
+    public float GetStamina() { return stamina; }
     public int GetPoints() { return points; }
 
     void Start()
     {
         originalHP = HP;
+        staminaMax = stamina;
         SpawnPlayer();
     }
 
@@ -45,6 +51,9 @@ public class PlayerController : MonoBehaviour
         ResetJump();
         ProcessMovement();
         ProcessJump();
+        IncrementStamina();
+
+        timeSinceUsedStamina += Time.deltaTime;
     }
 
     void ResetJump()
@@ -69,6 +78,12 @@ public class PlayerController : MonoBehaviour
         }
         else currentSpeed = playerSpeed;
 
+        if (IsSprinting() && stamina != 0 && movement != Vector3.zero)
+        {
+            currentSpeed *= sprintCoefficient;
+            DecrementStamina();
+        }
+
         characterController.Move(currentSpeed * Time.deltaTime * movement);
     }
 
@@ -83,6 +98,30 @@ public class PlayerController : MonoBehaviour
         playerVelocity.y -= gravity * Time.deltaTime;
 
         characterController.Move(Time.deltaTime * playerVelocity);
+    }
+
+    bool IsSprinting()
+    {
+        return Input.GetKey(KeyCode.LeftShift);
+    }
+
+    void IncrementStamina()
+    {
+        if (timeSinceUsedStamina > staminaRechargeStartTime)
+        {
+            stamina += staminaRechargeRate * Time.deltaTime;
+        }
+
+        stamina = Mathf.Clamp(stamina, 0f, staminaMax);
+        UpdateUI();
+    }
+
+    void DecrementStamina()
+    {
+        timeSinceUsedStamina = 0;
+        stamina -= sprintCost * Time.deltaTime;
+        stamina = Mathf.Clamp(stamina, 0f, staminaMax);
+        UpdateUI();
     }
 
     void EquipWeapon(Gun weapon)
