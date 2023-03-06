3e882f2f45f4ee558d76a2e02553ac5c9d7dba8f
Jaiten Beck <jaitenbeck@yahoo.com>
2023-03-05 17:57:13 -0700

    Update Enemy AI Script - Serialize Fields
    Added Serialized Fields for Enemy Stats as well as the Nav Mesh Agent and Model

--- GDB229-O/Assets/Scripts/EnemyAI.cs
+++ GDB229-O/Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
+    [Header("----- Components -----")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("----- Enemy Stats -----")]
     [SerializeField] int HP;
+    [SerializeField] float attackSpeed;
+    [SerializeField] int attackRange;
+    [SerializeField] GameObject projectile;
+    [SerializeField] int projectileSpeed;
+    [SerializeField] Transform shootPos;
 
     // Start is called before the first frame update
     void Start()
