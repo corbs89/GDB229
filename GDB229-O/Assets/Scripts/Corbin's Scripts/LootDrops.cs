a828a2ad988c8d9cceff46538018860aff410584
corbs89 <ccmein@student.fullsail.edu>
2023-03-09 03:25:56 -0600

    LootDrops
    Loot Drop script can be added to enemies to have them drop items on death

---
+++ GDB229-O/Assets/Scripts/Corbin's Scripts/LootDrops.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LootDrops : MonoBehaviour
+{
+    [SerializeField][Range(0, 100)] int dropChance;
+    [SerializeField] List<GameObject> lootDrops;
+
+    void OnDestroy()
+    {
+        int randomChance = Random.Range(0, 101);
+
+        if (randomChance <= dropChance)
+        {
+            int index = Random.Range(0, lootDrops.Count);
+            Debug.Log(randomChance);
+            GameObject item = Instantiate(lootDrops[index]);
+            item.transform.position = transform.position + new Vector3(0f, 1f, 0f);
+        }
+        
+    }
+}
